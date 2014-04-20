37a975e30936dbac32a6e4861d23e01d48c10acd
Johan Merello <johan@Pcdiseno.(none)>
2014-04-20 18:58:10 -0500

    first commit

---
+++ animsekai/codigo.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+
+	$(".li_regi").click(function(){
+
+
+		$(".registro").css({
+
+				"display":"block"
+
+		});
+
+	});
+
+
+	$(".cerrar_submit").click(function(){
+
+
+		$(".registro").css({
+
+				"display":"none"
+
+		});
+
+	});
+
+});
